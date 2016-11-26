003c7aa399fcd60e705224549b8250fda4788b85
Christian Naths <christiannaths@gmail.com>
2016-11-25 21:12:23 -0700

    adds team randomization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Team from './components/team'
 
+function shuffle(a) {
+  for (let i = a.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+  return a
+}
+
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -17,10 +25,17 @@ class App extends React.Component {
     }
   }
 
+  handleShuffle() {
+    this.setState({
+      players: shuffle(this.state.players)
+    })
+  }
+
   render(){
     const { players } = this.state
-    const team1 = players.slice(0, 3)
-    const team2 = players.slice(3)
+    const half = Math.ceil(players.length/2)
+    const team1 = players.slice(0, half)
+    const team2 = players.slice(half)
 
     return (
       <div className="app">
@@ -28,7 +43,9 @@ class App extends React.Component {
           <Team name='all-stars' players={team1} />
           <Team name='renegades' players={team2} />
         </div>
-        <button>Shuffle!</button>
+        <button onClick={this.handleShuffle.bind(this)}>
+          Shuffle!
+        </button>
       </div>
     );
   }
