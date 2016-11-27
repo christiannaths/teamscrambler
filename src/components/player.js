4dfa5e7a26c8d045b2f935d7af41fb60b2148233
Christian Naths <christiannaths@gmail.com>
2016-11-27 12:06:57 -0700

    adds in-place edit UI for players

--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-const Player = ({player}) => (
-  <li className="player">{player.name}</li>
-)
+class Player extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      editing: false,
+    }
+
+    this.handleEditToggle = this.handleEditToggle.bind(this)
+  }
+
+  handleEditToggle(){
+    const { editing } = this.state
+    this.setState({
+      editing: !editing
+    })
+  }
+
+  render(){
+    const { player } = this.props
+    const { editing } = this.state
+
+    return (
+      editing
+        ? <li>
+            <input defaultValue={player.name} />
+            <button onClick={this.handleEditToggle}>Done</button>
+          </li>
+        : <li className="player" onClick={this.handleEditToggle}>
+            {player.name}
+          </li>
+    )
+  }
+}
 
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
